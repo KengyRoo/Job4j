296954c61d3c37a3ef004deb3defce9e8adc92d8
KengyRoo <KengyRoo@mail.ru>
2020-01-28 15:35:13 +0300

    5.5. Простое число (1)

--- chapther_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapther_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,16 +3,12 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int finish) {
         boolean prime = true;
-        if (finish<2) {
-            prime = false;
-        }
-        else {
             for (int num = 2; num < finish; num++) {
                 if (finish % num == 0) {
                     prime = false;
+                    break;
                 }
             }
-        }
         return prime;
     }
 }
